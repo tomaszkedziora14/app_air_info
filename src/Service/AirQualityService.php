513422959f3535fb9709f8f61bc69cf963627f41
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2021-01-13 23:35:54 +0100

    crated new properties

--- src/Service/AirQualityService.php
+++ src/Service/AirQualityService.php
@@ -6,5 +6,31 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AirQualityService
 {
+    /**
+     * method GET
+     *
+     * @var string
+     */
+    const METHOD_GET = 'GET';
 
+    /**
+     * api client
+     *
+     * @var object
+     */
+    private $client;
+
+    /**
+     *  helper for array
+     *
+     * @var object
+     */
+    private $helper;
+
+    /**
+     * api url
+     *
+     * @var array
+     */
+    private $url = 'http://api.gios.gov.pl/pjp-api/rest/station/findAll';
 }
